246b9a3e68bedc8ff10776698eff43302d26d191
SudhaM15 <Sudharshan2515@gmail.com>
2023-09-05 00:25:24 +0530

    initialize portfolio

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        kav: "'Kaushan Script', cursive;",
+        oswald: "'Oswald', sans-serif;",
+        sand: "'Quicksand', sans-serif;",
+      },
+    },
+    container: {
+      center: true,
+      padding: "1rem",
+      screens: {
+        lg: "1124px",
+        xl: "1124px",
+        "2xl": "1124px",
+      },
+    },
+  },
+  plugins: [],
+};
